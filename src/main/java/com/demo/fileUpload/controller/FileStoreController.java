ead6417d169119231c489b5464abdf92082eac6f
Tarun <30947930+Tarun-Rana@users.noreply.github.com>
2022-10-05 18:49:58 +0530

    Update FileStoreController.java

--- src/main/java/com/demo/fileUpload/controller/FileStoreController.java
+++ src/main/java/com/demo/fileUpload/controller/FileStoreController.java
@@ -15,23 +15,18 @@ import org.bson.types.Binary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Mono;
-
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.awt.image.BufferedImage;
 import java.io.*;
-import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -46,26 +41,32 @@ public class FileStoreController {
     @Autowired
     FileStoreRepository fileStoreRepository;
     VideoStreamService videoStreamService;
-    ResourceLoader resourceLoader;
+
+    private String resourcePath="/Users/tarun/Documents/GitHub/digitalSignageBoard/src/main/resources";
 
     @RequestMapping(value="savefile",method=RequestMethod.POST)
-    public StringResponse saveimage(@RequestParam MultipartFile file,
-                                    HttpSession session) throws Exception{
+    public StringResponse saveimage(@RequestParam MultipartFile file) throws IOException {
         FileStore fileStore = new FileStore();
-        ServletContext context = session.getServletContext();
-        String path = ("/Users/tarun/Documents/GitHub/digitalSignageBoard/src/main/resources/images");
         String filename = file.getOriginalFilename().replaceAll(" ","_");
         fileStore.setFileSize(file.getSize()+"");
         fileStore.setFilename(filename);
         fileStore.setFileType(file.getContentType());
         fileStore.setCreateTimestamp(LocalDateTime.now());
-        fileStore.setFilePath("images/"+filename);
+        if(fileStore.getFileType().contains("pdf")){
+            return savePdf(file,fileStore,filename);
+        }else if(fileStore.getFileType().contains("mp4")){
+            return saveVideo(file,fileStore,filename); }
+       return saveImages(file,fileStore,filename);
+    }
+
+    private StringResponse saveVideo(MultipartFile file, FileStore fileStore, String filename) throws IOException {
+        String path = resourcePath+"/videos";
+        fileStore.setFilePath("videos/"+filename);
         fileStoreRepository.save(fileStore);
         System.out.println(path+"/"+filename);
-
         byte[] bytes = file.getBytes();
         BufferedOutputStream stream =new BufferedOutputStream(new FileOutputStream(
-                new File(path + File.separator + filename)));
+                path + File.separator + filename));
         stream.write(bytes);
         stream.flush();
         stream.close();
@@ -75,32 +76,18 @@ public class FileStoreController {
         return stringResponse;
     }
 
-    @RequestMapping(value="savePDF",method=RequestMethod.POST)
-    public StringResponse savePDF(@RequestParam MultipartFile file,
-                                    HttpSession session) throws Exception{
-
-
-        FileStore fileStore = new FileStore();
-        ServletContext context = session.getServletContext();
-        String path = ("/Users/tarun/Documents/GitHub/digitalSignageBoard/src/main/resources/pdf");
-        String filename = file.getOriginalFilename().replaceAll(" ","_");
-        fileStore.setFileSize(file.getSize()+"");
-        fileStore.setFilename(filename);
-        fileStore.setFileType(file.getContentType());
+    public StringResponse savePdf(MultipartFile file,FileStore fileStore,String filename) throws IOException {
+        String path = resourcePath+"/pdf";
         fileStore.setCreateTimestamp(LocalDateTime.now());
         fileStore.setFilePath("pdf/"+filename);
-
         System.out.println(path+"/"+filename);
-
         byte[] bytes = file.getBytes();
         BufferedOutputStream stream =new BufferedOutputStream(new FileOutputStream(
-                new File(path + File.separator + filename)));
+                path + File.separator + filename));
         stream.write(bytes);
         stream.flush();
         stream.close();
-
-        PDDocument document = PDDocument.load(new File("/Users/tarun/Documents/GitHub/digitalSignageBoard" +
-                "/src/main/resources/"+fileStore.getFilePath()));
+        PDDocument document = PDDocument.load(new File(resourcePath+"/"+fileStore.getFilePath()));
         fileStore.setCount(document.getNumberOfPages());
         fileStoreRepository.save(fileStore);
         PDFRenderer pdfRenderer = new PDFRenderer(document);
@@ -108,13 +95,30 @@ public class FileStoreController {
             BufferedImage bim = pdfRenderer.renderImageWithDPI(
                     page, 300, ImageType.RGB);
             ImageIOUtil.writeImage(
-                    bim, String.format("/Users/tarun/Documents/GitHub/digitalSignageBoard/src/main/resources/pdf" +
+                    bim, String.format(resourcePath+"/pdf" +
                             "/"+fileStore.getFilename()+"-%d.%s", page + 1, "png"), 300);
         }
         document.close();
 
         StringResponse stringResponse = new StringResponse();
         stringResponse.setResponse(fileStore.getId()+" "+fileStore.getCount());
+        return stringResponse;
+    }
+    public StringResponse saveImages(MultipartFile file,FileStore fileStore,String filename) throws IOException {
+
+        String path = resourcePath+"/images";
+        fileStore.setFilePath("images/"+filename);
+        fileStoreRepository.save(fileStore);
+        System.out.println(path+"/"+filename);
+        byte[] bytes = file.getBytes();
+        BufferedOutputStream stream =new BufferedOutputStream(new FileOutputStream(
+                new File(path + File.separator + filename)));
+        stream.write(bytes);
+        stream.flush();
+        stream.close();
+
+        StringResponse stringResponse = new StringResponse();
+        stringResponse.setResponse(fileStore.getId());
         return stringResponse;
     }
 
@@ -128,104 +132,56 @@ public class FileStoreController {
         var imgFile = new ClassPathResource(fileStore.getFilePath()+"-"+data[1]+".png");
         response.setContentType(MediaType.IMAGE_JPEG_VALUE);
         StreamUtils.copy(imgFile.getInputStream(), response.getOutputStream());
-
     }
 
     @RequestMapping(value = "/downloadById/{id}", method = RequestMethod.GET,
             produces = MediaType.IMAGE_JPEG_VALUE)
-
     public void getImagebyId(@PathVariable String id,HttpServletResponse response) throws IOException {
-
-        //var imgFile = new ClassPathResource("images/photo-1543373014-cfe4f4bc1cdf.jpeg");
         FileStore fileStore = fileStoreRepository.findById(id).orElse(null);
-
-        var imgFile = new ClassPathResource(fileStore.getFilePath());
+        InputStream is = new FileInputStream(resourcePath+"/"+fileStore.getFilePath());
         response.setContentType(MediaType.IMAGE_JPEG_VALUE);
-        StreamUtils.copy(imgFile.getInputStream(), response.getOutputStream());
-
+        StreamUtils.copy(is, response.getOutputStream());
     }
 
-    @GetMapping("/stream/{fileName}")
-    public Mono<ResponseEntity<byte[]>> streamVideo(@RequestHeader(value = "Range", required = false) String httpRangeList,
-                                                    @PathVariable String id) {
+    @GetMapping(value = "download/{id}")
+    public ResponseEntity<Resource> getByID(@PathVariable("id") String id) throws IOException {
+        System.out.println(id);
+        String[] data = id.split(" ");
+        FileStore fileStore = fileStoreRepository.findById(data[0]).orElse(null);
+        InputStream is= null;
+        if(fileStore.getFileType().contains("pdf")){
+            is  = new FileInputStream(resourcePath+"/pdf/"+fileStore.getFilename()+"-"+data[1]+".png");
+            return ResponseEntity.ok()
+                    .contentType(MediaType.valueOf(MediaType.IMAGE_JPEG_VALUE))
+                    .body(new ByteArrayResource(is.readAllBytes()));
+        }else if(fileStore.getFileType().contains("mp4")) {
+            is = new FileInputStream(resourcePath + "/" + fileStore.getFilePath());
+            return ResponseEntity.ok().contentType(MediaType.APPLICATION_OCTET_STREAM).
+                    header("Content-Disposition", "attachment; filename="+fileStore.getFilename(),"video/mp4").
+                    body(new ByteArrayResource(is.readAllBytes()));
+        }
+        is = new FileInputStream(resourcePath + "/" + fileStore.getFilePath());
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(MediaType.IMAGE_JPEG_VALUE))
+                .body(new ByteArrayResource(is.readAllBytes()));
+    }
+    @GetMapping(value = "video/{id}", produces = "video/mp4")
+    public ResponseEntity<Resource> getVideoByName(@PathVariable("id") String id) throws IOException {
+        System.out.println(id);
         FileStore fileStore = fileStoreRepository.findById(id).orElse(null);
-        return Mono.just(videoStreamService.prepareContent(fileStore.getFilename(),
-                fileStore.getFileType(), httpRangeList));
-    }
-
-    @GetMapping(value = "video/{title}", produces = "video/mp4")
-    public ResponseEntity<Resource> getVideoByName(@PathVariable("title") String name) throws IOException {
-        System.out.println(name);
-        InputStream is = new FileInputStream("/Users/tarun/Documents/GitHub/digitalSignageBoard/src/main/resources/videos/vid.mp4");
+        InputStream is = new FileInputStream(resourcePath + "/" + fileStore.getFilePath());
         return ResponseEntity
                 .ok(new ByteArrayResource(is.readAllBytes()));
-    }
-
-    @GetMapping(
-            value = "/get-file/{id}",
-            produces = MediaType.APPLICATION_OCTET_STREAM_VALUE
-    )
-    public @ResponseBody void getFile(@PathVariable String id,HttpServletResponse response) throws IOException {
-        FileStore fileStore = fileStoreRepository.findById(id).orElse(null);
-        String path = "/Users/tarun/Documents/GitHub/digitalSignageBoard/target/"+fileStore.getFilePath();
-        InputStream in = getClass()
-                .getResourceAsStream(path);
-        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
-   //     StreamUtils.copy(inputStream, response.getOutputStream());
-        File initialFile = new File("/Users/tarun/Documents/GitHub/digitalSignageBoard/src/main/resources/"+
-                fileStore.getFilePath());
-        InputStream targetStream = FileUtils.openInputStream(initialFile);
-        StreamUtils.copy(targetStream, response.getOutputStream());
     }
 
     @RequestMapping(value = "/downloadAll", method = RequestMethod.GET)
 
     public List<FileStore> getAllImages() throws IOException {
-
-        //var imgFile = new ClassPathResource("images/photo-1543373014-cfe4f4bc1cdf.jpeg");
         List<FileStore> fileStores = new ArrayList<>();
         fileStores=fileStoreRepository.findAll();
         return fileStores;
     }
 
-    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<Object> createAppreciation(
-            @RequestPart(value = "files", required = true) MultipartFile[] files) {
-
-        FileStore fileStore = new FileStore();
-        try {
-
-            fileStore.setCreateTimestamp(LocalDateTime.now());
-
-            Image image = new Image();
-            image.setId(fileStore.getId());
-            List<Binary> binaryList = new ArrayList<>();
-            List<String> encodedImageList = new ArrayList<String>();
-
-            Arrays.asList(files).stream().forEach(file -> {
-                try {
-                    String string =Base64.getEncoder().encodeToString(file.getBytes()).replaceAll("\"", "");
-                    encodedImageList.add(string);
-                    log.info(encodedImageList.get(0));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-
-            image.setImagefiles(binaryList);
-            image.setEncodedImageString(encodedImageList);
-            fileStore.setImage(image);
-            FileStore fileStored = fileStoreRepository.save(fileStore);
-            Image createdImage = fileStored.getImage();
-            createdImage.setImagefiles(new ArrayList<Binary>());
-            fileStored.setImage(createdImage);
-
-            return new ResponseEntity<>(fileStored, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.EXPECTATION_FAILED);
-        }
-
-    }
     @GetMapping("/getAllUploads")
     public ResponseEntity<List<FileStore>> getAllAppreciation() {
         try {
